fe1ce84b7f2062c019d76b46be7f75170cc8ba8c
dh1977 <97996105+dh1977@users.noreply.github.com>
2022-04-19 18:29:40 -0400

    Delete vistaOT.sql

--- vistaOT.sql
+++
@@ -1,9 +0,0 @@
-SELECT ot.nId, ot.fProgramada, ot.fEjecucion, ot.cObservaciones,
-		 cm.nContrato, cm.fDesde, cm.fHasta, cm.nPeriodicidad,
-		 c.nId nIdClic.nRut, c.cNombreCli, c.cDireccionCli, c.cFonoCli, c.cEmailCli,
-		 p.nId nIdSupervisor, p.cNombre cNombreSupervisor
-	FROM OrdenTrabajo ot
-		JOIN ContratoMantencion cm ON cm.nContrato = ot.nContrato
-		JOIN Cliente c ON c.nId = cm.nIdCliente
-		JOIN Personal p ON p.nId = ot.nIdSupervisor
-	
